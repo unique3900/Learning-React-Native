f933d40845fb95a48bf7cd88e64d18b172e28961
parashar <parasharneupane3900@gmail.com>
2023-11-20 08:54:30 +0545

    Basic App Develop For Web

--- BasicApp/App.js
+++ BasicApp/App.js
@@ -1,11 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View,Image, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{
+      width: '100%',
+      height:'100vh',
+      display: 'flex',
+      marginTop:'80px',
+      flexDirection: 'column',
+      alignItems:'center'
+    }}>
+      <Text style={{textAlign:'center'}}>Random Text</Text>
+      <View>
+        <Image  source={{
+            uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
+        }}
+        style={{width: 200, height: 200}}
+        />
+        <Text>Some Another Random Texts</Text>
+        <Button title='Take me to the moon'/>
+      </View>
     </View>
   );
 }
